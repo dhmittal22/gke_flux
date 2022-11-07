41c67f00b4032a5e47dcb042a0f0e646ac4b9b11
dhmittal22 <110419772+dhmittal22@users.noreply.github.com>
2022-11-07 13:35:32 +0530

    Add files via upload

---
+++ var.tf
@@ -0,0 +1,55 @@
+variable "project_id" {
+  description = "project id"
+  type        = string
+}
+
+variable "github_token" {
+  description = "token for github"
+  type        = string
+}
+
+variable "github_owner" {
+  description = "github owner"
+  type        = string
+}
+
+variable "repository_name" {
+  description = "repository name"
+  type        = string
+}
+
+variable "branch" {
+  description = "branch"
+  type        = string
+}
+
+variable "target_path" {
+  type        = string
+  description = "Relative path to the Git repository root where the sync manifests are committed."
+}
+
+variable "flux_namespace" {
+  type        = string
+  description = "the flux namespace"
+}
+
+variable "cluster_name" {
+  type        = string
+  description = "cluster name"
+}
+
+variable "cluster_region" {
+  type        = string
+  description = "cluster region"
+}
+
+variable "use_private_endpoint" {
+  type        = bool
+  description = "Connect on the private GKE cluster endpoint"
+  default     = false
+}
+
+variable "github_deploy_key_title" {
+  type        = string
+  description = "Name of github deploy key"
+}
